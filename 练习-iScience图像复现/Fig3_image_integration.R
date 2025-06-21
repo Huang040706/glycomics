59d97727af65d20fe19706ed8891212eacf5048e
Huang Junqi <13817670946@163.com>
2025-06-21 12:54:08 +0800

    Delete 练习-iScience图像复现 directory

--- 练习-iScience图像复现/Fig3_image_integration.R
+++
@@ -1,7 +0,0 @@
-p1 = image_read("valcano_plot_A.png")
-p2 = image_read("valcano_plot_B.png")
-
-pp1 = image_ggplot(p1,interpolate = F)
-pp2 = image_ggplot(p2,interpolate = F)
-
-ggarrange(pp1,pp2,ncol = 1)
